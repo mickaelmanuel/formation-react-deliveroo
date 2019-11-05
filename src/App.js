4a95a0ac9cb3aaeedb96035f5e03c16649e594ae
mickaelmanuel <mickael.manuel@adevinta.com>
2019-11-05 14:29:07 +0100

    useeffect

--- src/App.js
+++ src/App.js
@@ -20,67 +20,65 @@ const mapDispatchToProps = {
   fetchMyData
 };
 
-class AppRender extends React.Component {
-  componentDidMount() {
-    this.props.fetchMyData();
+const AppRender = ({ dataHasError, data, panier, incrementItem, addItem, fetchMyData }) => {
+  React.useEffect(() => {
+    fetchMyData();
+  });
+
+  if (dataHasError) {
+    return <p>Ouuups! Une erreur est arrivée lors du chargement des données</p>;
   }
 
-  render() {
-    if (this.props.dataHasError) {
-      return <p>Ouuups! Une erreur est arrivée lors du chargement des données</p>;
-    }
+  // if (dataIsLoading) {
+  //   return <p>Loading…</p>;
+  // }
 
-    // if (this.props.dataIsLoading) {
-    //   return <p>Loading…</p>;
-    // }
+  return (
+    <div>
+      <Header restaurant={data && data.restaurant} />
+      <div className="Content">
+        <div className="Content--center">
+          <div className="Menu">
+            {data &&
+              data.menu &&
+              Object.keys(data.menu).map((menuName, index) => {
+                return (
+                  data.menu[menuName].length > 0 && (
+                    <Menu title={menuName} key={index}>
+                      {data.menu[menuName].map(item => {
+                        return (
+                          <MenuItem
+                            key={item.id}
+                            title={item.title}
+                            description={item.description}
+                            price={parseFloat(item.price)}
+                            popular={item.popular}
+                            picture={item.picture}
+                            onClick={() => {
+                              let index = panier.findIndex(x => x.id === item.id);
 
-    return (
-      <div>
-        <Header restaurant={this.props.data && this.props.data.restaurant} />
-        <div className="Content">
-          <div className="Content--center">
-            <div className="Menu">
-              {this.props.data &&
-                this.props.data.menu &&
-                Object.keys(this.props.data.menu).map((menuName, index) => {
-                  return (
-                    this.props.data.menu[menuName].length > 0 && (
-                      <Menu title={menuName} key={index}>
-                        {this.props.data.menu[menuName].map(item => {
-                          return (
-                            <MenuItem
-                              key={item.id}
-                              title={item.title}
-                              description={item.description}
-                              price={parseFloat(item.price)}
-                              popular={item.popular}
-                              picture={item.picture}
-                              onClick={() => {
-                                let index = this.props.panier.findIndex(x => x.id === item.id);
-
-                                if (index !== -1) {
-                                  this.props.incrementItem(item.id);
-                                } else {
-                                  this.props.addItem({ id: item.id, title: item.title, price: item.price });
-                                }
-                              }}
-                            />
-                          );
-                        })}
-                      </Menu>
-                    )
-                  );
-                })}
-            </div>
-            <div className="Cart">
-              <Cart />
-            </div>
+                              if (index !== -1) {
+                                incrementItem(item.id);
+                              } else {
+                                addItem({ id: item.id, title: item.title, price: item.price });
+                              }
+                            }}
+                          />
+                        );
+                      })}
+                    </Menu>
+                  )
+                );
+              })}
+          </div>
+          <div className="Cart">
+            <Cart />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export const App = connect(
   mapStateToProps,
